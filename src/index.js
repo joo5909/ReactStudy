88864110e10896a596e77b307b7fb426d94c4adc
Dan <joo5909@naver.com>
2020-02-19 13:21:53 +0900

    -

--- src/index.js
+++ src/index.js
@@ -1,78 +1,11 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-  const Title = () =>{  
-    return (
-      <span>React</span>
-      )
-  }
-
-  const Animal = ({ name }) => (
-    <li>
-      <span>{name}</span>
-    </li>
-  )
-  
-  
-  const Animals = () => {
-    const animalList = ['dog', 'cat', 'tiger']
-  
-    return (
-      <ol>
-        {animalList.map((animal, index) => (
-          <Animal key={index} name={animal} />
-        ))}
-      </ol>
-    )
-  }
-  
-  const App2 = () => {
-    const logNickname = event => {
-      const nickname = event.target.value
-      console.log(nickname)
-      console.log('nickname length: ', nickname.length)
-    }
-    return(
-    <Fragment>
-      <button onClick={event => console.log('clicked1!')}>click1!</button>
-      <button onClick={event => console.log('clicked2!')}>click2!</button>     
-      <input type='text' onChange={event => console.log(event.target.value)} />
-      <input type='text' onChange={logNickname} />
-    </Fragment>
-    )
-
-
-
-
-    }
-    const Counter = () => {
-      const [nickname, setNickname] = useState('Danuel')
-      const [count, setCount] = useState(0)
-    
-      const decreaseCount = () => {
-        setCount(count - 1)
-      }
-      const increaseCount = () => {
-        setCount(count + 1)
-      }
-    
-      useEffect(() => {
-        document.title = count
-      })
-    
-      return (
-        <div>
-          <p>{nickname}</p>
-          <p>{count}</p>
-          <button onClick={decreaseCount}>- 1</button>
-          <button onClick={increaseCount}>+ 1</button>
-        </div>
-      )
-    }
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ 
+ReactDOM.render(<App />, document.getElementById('root'));
 //ReactDOM.render(<Title/>, document.getElementById('title'));
 
 // ReactDOM.render(<App />, document.getElementById('root'));
