d1df72c43cff06ce40095c78a6c310f23cf81112
Dan <joo5909@naver.com>
2020-02-27 17:51:27 +0900

    cssStudy

--- src/Contents/Content.js
+++ src/Contents/Content.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import '../css/heart.css';
 class Content extends React.Component {
     render(){
         return (
             <div>
                 <h2>{this.props.contentTitle}</h2>
                 <p>{this.props.contentBody}</p>
+                <div with="50%">
+                <div class="back"></div>
+                <div class="heart"></div>
+                </div>
+
             </div>
         );
     }
